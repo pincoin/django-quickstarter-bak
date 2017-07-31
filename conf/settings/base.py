586aee1cdc752c09db9919b764251f842f313727
mairoo <pincoins@gmail.com>
2017-07-31 10:16:50 +0900

    templates path fix

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
