fdc9eef17e7d54ae8ee0bee395c05f7f19dedcf4
mairoo <pincoins@gmail.com>
2017-07-31 12:06:19 +0900

    urlpatterns for media root and static root

--- conf/urls.py
+++ conf/urls.py
@@ -1,6 +1,8 @@
+from django.conf import settings
 from django.conf.urls import (
     url, include
 )
+from django.conf.urls.static import static
 from django.contrib import admin
 from registration.backends.hmac.views import RegistrationView
 
@@ -17,3 +19,7 @@ urlpatterns = [
     url(r'^accounts/',
         include('registration.backends.hmac.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
